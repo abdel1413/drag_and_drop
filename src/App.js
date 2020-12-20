055ba1534aab1cdd348aac6502309549fe9d6aad
abdel <aboualyet63@gmail.com>
2020-12-20 15:23:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import DesignContainer from "./components/DesignContainer";
+
+function App(props) {
+  return <DesignContainer />;
+}
+
+export default App;
